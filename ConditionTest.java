d1c3c67fb6f4a5117d6725ebbe614a3a6dd77744
dlee <lbqq0000@gmail.com>
2017-02-24 11:13:26 +0800

    naive version

---
+++ ConditionTest.java
@@ -0,0 +1,63 @@
+import java.util.Random;
+
+/*
+ * 考虑这样一个问题, 两个线程t1, t2
+ * t1生成一堆随机数, 往一个累加器上累加, 当累加结果达到阈值, 就退出
+ *
+ * t2要打印达到阈值后的结果
+ *
+ * 并且我们假设, t1和t2是两个人开发的, t2无法得知这个阈值是多少, 即t2不能自己去检测这个值
+ * */
+
+public class ConditionTest {
+    private Integer x = 0;
+    private boolean shouldPrint = false;
+
+    public static void main(String argv[]) {
+        ConditionTest y = new ConditionTest();
+        y.demo();
+    }
+
+    public void demo() {
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    if (shouldPrint) {
+                        System.out.println(x);
+                        break;
+                    }
+                }
+            }
+        });
+
+        t2.start();
+        
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int max = 1024;
+                while (true) {
+                    Random rand = new Random();
+                    int n = rand.nextInt(100) + 1;
+                    System.out.println("generate n:" + n);
+                    x += n;
+                    if (x > max) {
+                        shouldPrint = true;
+                        break;
+                    } else {
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                        }
+                    }
+                }
+                System.out.println("done my own work!");
+            }
+        });
+
+        t1.start();
+    }
+}
+
